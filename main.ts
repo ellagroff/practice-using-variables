2f2fbfce84a83a0c8f18b7e0281fbe870fdc6341
ellagroff <122560331+ellagroff@users.noreply.github.com>
2023-01-26 13:15:25 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    number += 1
+    basic.showNumber(number,25)
+})
+input.onButtonPressed(Button.AB, function () {
+    number = 0
+    basic.showNumber(number,25)
+})
+input.onButtonPressed(Button.B, function () {
+    number += -1
+    basic.showNumber(number,25)
+})
+let number = 0
+number = 0
+basic.showNumber(number,25)
